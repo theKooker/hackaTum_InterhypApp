47d6fe1f096b29d308c0572b93d4fcb50bb149d9
alvandi <geradylanalvandi@gmail.com>
2021-11-20 13:59:34 +0100

    commited main

--- shower_tracking_app/lib/main.dart
+++ shower_tracking_app/lib/main.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-<<<<<<< Updated upstream
 import 'package:shower_tracking_app/pages/splash_screen.dart';
 import 'package:shower_tracking_app/router.dart';
-=======
+
 import 'package:shower_tracking_app/components/bottom_bar.dart';
 import 'package:shower_tracking_app/components/history.dart';
 import 'package:shower_tracking_app/components/ranking.dart';
@@ -12,16 +11,13 @@ import 'package:shower_tracking_app/components/tracker.dart';
 import 'dart:ffi' as ffi;
 import 'package:path/path.dart' as path;
 
->>>>>>> Stashed changes
 
 // FFI signature of the C function
 typedef SignalProcessFunc = ffi.Void Function();
 // Dart type definition for calling the C foreign function
 typedef SignalProcess = void Function();
 void main() {
-<<<<<<< Updated upstream
   runApp(const App());
-=======
     // Open the dynamic library
   var libraryPath =
       path.join(Directory.current.path, 'hello_library', 'hello.def');
@@ -35,7 +31,6 @@ void main() {
   // Call the function
   signalProcess();
   runApp(const MyApp());
->>>>>>> Stashed changes
 }
 
 class App extends StatefulWidget {
